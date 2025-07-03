91032938013f6d91c99319ec8a39e9d501c13ed3
ogasawara-ryou <ogasawara.ryou@tokyo-gas.co.jp>
2025-07-03 15:36:52 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+const pushBtn = document.getElementById('btn');
+
+pushBtn.addEventListener('click' , () => {
+
+    const textline = document.getElementById('text');
+    textline.textContent ='ボタンをクリックしました';
+    
+
+});
+
+
+
